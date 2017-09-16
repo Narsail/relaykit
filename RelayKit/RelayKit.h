a514e04a64e7bb5e34a28e99612bf020f2eaa7ae
David Moeller <david.moeller@evomo.de>
2017-09-16 13:33:01 +0200

    Initial Commit

---
+++ RelayKit/RelayKit.h
@@ -0,0 +1,19 @@
+//
+//  RelayKit.h
+//  RelayKit
+//
+//  Created by David Moeller on 16.09.17.
+//  Copyright © 2017 David Moeller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RelayKit.
+FOUNDATION_EXPORT double RelayKitVersionNumber;
+
+//! Project version string for RelayKit.
+FOUNDATION_EXPORT const unsigned char RelayKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RelayKit/PublicHeader.h>
+
+
